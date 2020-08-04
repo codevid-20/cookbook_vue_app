3ef0ffa920b62b30875495fe08e5f5018882cc6d
brian <riceb53@gmail.com>
2020-08-03 21:07:57 -0400

    run index automatically, show recipes in v for

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="home">
-    <button v-on:click="indexRecipes()">Get some data from the api</button>
+
     
     <h1>{{ message }}</h1>
     <h1>{{ name }}</h1>
-    <h1>{{ recipes }}</h1>
+
+    <div v-for="recipe in recipes">
+      <p>title: {{ recipe.title }}</p>
+      <p>ingredients: {{ recipe.ingredients }}</p>
+      <p>image_url: {{ recipe.image_url }}</p>
+      <hr>
+    </div>
+    
   </div>
 </template>
 
@@ -22,14 +29,14 @@ export default {
       recipes: []
     };
   },
-  created: function() {},
+  created: function() {
+    this.indexRecipes();
+    
+  },
   methods: {
     indexRecipes: function() {
       console.log("why won't you print out!!!");
       console.log('recipes index...');
-
-      
-
       axios.get('/api/recipes').then(response => {
         console.log(response);
         this.recipes = response.data;

d1c3009a6f2b7c5cea2f0c38e36dabc674347aa9
brian <riceb53@gmail.com>
2020-08-03 21:42:21 -0400

    hardcode new/create

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,9 @@
 
     
     <h1>{{ message }}</h1>
-    <h1>{{ name }}</h1>
+    <p>Make a new recipe</p>
+    <button v-on:click="addRecipe()">Add Recipe</button>
+    
 
     <div v-for="recipe in recipes">
       <p>title: {{ recipe.title }}</p>
@@ -43,6 +45,22 @@ export default {
       axios.get('/api/recipes').then(response => {
         console.log(response);
         this.recipes = response.data;
+      })
+    },
+    addRecipe: function() {
+      console.log('adding recipe...');
+
+      var params = {
+        title: "THIS IS A BRAND NEW RECIPE!!!",
+        prep_time: 200,
+        directions: "here are the directions",
+        ingredients: "here are the ingredients",
+        image_url: "http://something.com"
+      }
+
+      axios.post('/api/recipes', params).then(response => {
+        console.log(response.data);
+        this.recipes.push(response.data);
       })
     }
   }

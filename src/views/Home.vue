edb651fadae0d3ea479956e02d4e5a7b21d00992
brian <riceb53@gmail.com>
2020-08-03 20:29:56 -0400

    GET DATA FROM RAILS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
+    <button v-on:click="indexRecipes()">Get some data from the api</button>
+    
     <h1>{{ message }}</h1>
     <h1>{{ name }}</h1>
+    <h1>{{ recipes }}</h1>
   </div>
 </template>
 
@@ -15,10 +18,23 @@ export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      name: "brian"
+      name: "brian",
+      recipes: []
     };
   },
   created: function() {},
-  methods: {}
+  methods: {
+    indexRecipes: function() {
+      console.log("why won't you print out!!!");
+      console.log('recipes index...');
+
+      
+
+      axios.get('/api/recipes').then(response => {
+        console.log(response);
+        this.recipes = response.data;
+      })
+    }
+  }
 };
 </script>
